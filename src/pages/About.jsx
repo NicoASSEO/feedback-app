d644b5362dc5109fa4d472a127dc7db90f27a228
nicolasasseo <n.asseo23@ejm.org>
2024-08-10 18:23:33 +0200

    Added routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Card from "../components/shared/Card"
+
+const About = () => {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>
+          This is a React project to leave feedback for a product or service
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default About
